a9af02227a4869f65c7279192db915373a466ea9
Ansar Butt <41417253+ansarbutt@users.noreply.github.com>
2018-09-15 17:37:15 -0400

    Removing Fields
    Removing Cabin Letter and Ticket Prefix

--- vtreat Data Preparation.R
+++ vtreat Data Preparation.R
@@ -12,15 +12,7 @@ cols <- c("SibSp", "Parch", "FamilySize", "Pclass")
 data <- train
 # extract last name from data
 data$LastName <- sapply(strsplit(data$Name, ","), `[`, 1)
-data$CabinLetter <- substr(data$Cabin, 0, 1)
-data$TicketSplit <- sapply(strsplit(data$Ticket, " "), `[`, 1)
-data$NumericTicket <- as.character(as.numeric(data$TicketSplit))
-data$TicketPrefix <- ifelse(is.na(data$NumericTicket), data$TicketSplit , "")
-# convert column list to character for processing to categorical
 data[cols] <- lapply(data[cols], as.character)
-data$Name <- NULL
-data$TicketSplit <- NULL
-data$NumericTicket <- NULL
 
 vars <- setdiff(colnames(data),c('PassengerId'))
 # apply vtreat cross validation to develop treatments
@@ -40,13 +32,6 @@ write.csv(dTrainTreated, file = "C:/Users/User/Desktop/Titanic/dTrainTreated.csv
 test <- read_csv("C:/Users/User/Desktop/Titanic/scoring_before_processing.csv")
 test[cols] <- lapply(test[cols], as.character)
 test$LastName <- sapply(strsplit(test$Name, ","), `[`, 1)
-test$CabinLetter <- substr(test$Cabin, 0, 1)
-test$TicketSplit <- sapply(strsplit(test$Ticket, " "), `[`, 1)
-test$NumericTicket <- as.character(as.numeric(test$TicketSplit))
-test$TicketPrefix <- ifelse(is.na(test$NumericTicket), test$TicketSplit , "")
-test$Name <- NULL
-test$TicketSplit <- NULL
-test$NumericTicket <- NULL
 dTestTreated <- vtreat::prepare(treatments,test)
 dTestTreated$PassengerId <- test$PassengerId
 # export treated test set to csv
